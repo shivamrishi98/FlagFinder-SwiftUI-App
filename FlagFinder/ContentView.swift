10020cc7817d1dd1973220026aaabcb3eef27bbb
shivamrishi98 <shivamrishisr@gmail.com>
2020-07-14 12:43:12 +0530

    Initial Commit

---
+++ FlagFinder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlagFinder
+//
+//  Created by Shivam Rishi on 14/07/20.
+//  Copyright © 2020 shivam. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
